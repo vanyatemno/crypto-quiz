bb1d3450c21f2054ce4576895ad4e6f2cca795ab
Ivan <ivan.markhaichuk@ukma.edu.ua>
2022-10-25 01:39:52 +0300

    minimal version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import SuccessFrame from "./frames/success-frame";
+import FailureFrame from "./frames/failure-frame";
+import StartFrame from "./frames/start-frame";
+import ResultsFrame from "./frames/results-frame";
+import QuizFrame from "./frames/quiz-frame";
+import {QuizDataContextProvider} from "./contexts/quiz-data-context";
+import {UserDataProvider} from "./contexts/user-data-context";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import EmailFrame from "./frames/email-frame";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <UserDataProvider>
+            <QuizDataContextProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index path={"/"} element={<StartFrame/>}/>
+                        <Route path={"/question"} element={<QuizFrame/>}/>
+                        <Route path={"/success"} element={<SuccessFrame/>}/>
+                        <Route path={"/failure"} element={<FailureFrame/>}/>
+                        <Route path={"/results"} element={<ResultsFrame/>}/>
+                        <Route path={"/answers"} element={<EmailFrame/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </QuizDataContextProvider>
+        </UserDataProvider>
+
+
+
     </div>
   );
 }
